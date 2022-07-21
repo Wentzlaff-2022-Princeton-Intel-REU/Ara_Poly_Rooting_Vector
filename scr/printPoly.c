5278f2b1281980cf43430db23e5e6f2e9d75840f
Mukund Ramakrishnan <mramakri@outlook.com>
2022-07-21 18:48:29 -0400

    First attempt at ara-izing

---
+++ scr/printPoly.c
@@ -0,0 +1,17 @@
+/*--------------------------------------------------------------------*/
+/* newton.c                                                           */
+/*--------------------------------------------------------------------*/
+
+#include "printf.h"
+// #include <stdio.h>
+#include "printPoly.h"
+
+/*--------------------------------------------------------------------*/
+
+void printPoly(Polynomial_t poly) {
+    // printf("Degreee: %d\n", poly.degree);
+    for (int i = poly.degree; i > 0; i--) {
+        printf("%.1f * x^%d + ", poly.coefficients[i], i);
+    }
+    printf("%.1f\n\n", poly.coefficients[0]);
+}
--- scr/vector/main.c
+++ scr/vector/main.c
@@ -3,17 +3,27 @@
 /*--------------------------------------------------------------------*/
 
 #include <float.h>
-#include "reading.h"
 #include "newton.h"
 #include "printf.h"
+#include "printPoly.h"
+#include "runtime.h"
 
 /*--------------------------------------------------------------------*/
 
 int main(int argc, char *argv[]) {
     double crit_conversion = 1e-14;
     
-    Polynomial_t poly = reading();
+     Polynomial_t poly;
+    poly.degree = 5;
+    double arr[] = {120, 94, -51, -23, 3, 1};
+    poly.coefficients = arr;
+
+printf("Polynomial:\n");
+    printPoly(poly);
+
+    start_timer();
     double* roots = newton(poly, crit_conversion);
+    end_timer();
 
     if (roots[0] == DBL_MAX) {
         printf("Your polynomial has no roots.\n");
@@ -26,6 +36,8 @@ int main(int argc, char *argv[]) {
             printf("The root approximation is: %.14lg \n", roots[i]);
         }
     }
+        printf("\nRuntime (cycles): %d\n", cycle_time);
+
     
     // for (int i = 0; i <= polyd.degree; i++) {
     //     printf("%lf \n", polyd.coefficients[i]);
