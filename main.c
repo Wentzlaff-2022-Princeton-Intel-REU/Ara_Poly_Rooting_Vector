c307b0baf3a693b50608c3991033b59ab0770c24
Dara Oseyemi <doseyemi@princeton.edu>
2022-07-22 13:42:43 -0400

    minor change

--- main.c
+++ main.c
@@ -13,12 +13,12 @@
 int main(int argc, char *argv[]) {
     double crit_conversion = 1e-14;
     
-     Polynomial_t poly;
+    Polynomial_t poly;
     poly.degree = 5;
     double arr[] = {120, 94, -51, -23, 3, 1};
     poly.coefficients = arr;
 
-printf("Polynomial:\n");
+    printf("Polynomial:\n");
     printPoly(poly);
 
     start_timer();
@@ -26,22 +26,18 @@ printf("Polynomial:\n");
     end_timer();
 
     if (roots[0] == DBL_MAX) {
-        printf("Your polynomial has no roots.\n");
+        printf("Your polynomial has no real roots.\n");
     }
     else {
         for (int i = 0; i < poly.degree; i++) {
             if (roots[i] == DBL_MAX) {
                 break;
             }
-            printf("The root approximation is: %.14lg \n", roots[i]);
+            printf("The root approximation is: %.3f \n", roots[i]);
         }
     }
-        printf("\nRuntime (cycles): %d\n", cycle_time);
 
-    
-    // for (int i = 0; i <= polyd.degree; i++) {
-    //     printf("%lf \n", polyd.coefficients[i]);
-    // }
+    printf("\nRuntime (cycles): %d\n", cycle_time);
 
     return 0;
 }
