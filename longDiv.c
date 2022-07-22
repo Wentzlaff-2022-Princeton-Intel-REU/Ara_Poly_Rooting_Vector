35a845b318af3032e2fb31daec664480866f6a7a
m-ukund <mramakri@outlook.com>
2022-07-21 20:29:12 -0400

    remove * from longDiv

--- longDiv.c
+++ longDiv.c
@@ -10,7 +10,7 @@
 
 Polynomial_t longDiv(Polynomial_t poly, double root, double diff) {
     int n = poly.degree - 1;
-    double* a_n [n + 1];
+    double a_n [n + 1];
 
     a_n[n] = poly.coefficients[n + 1];
     for (int i = n; i > 0; i--) {
