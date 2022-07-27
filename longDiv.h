0e6e5edda084475f7601a8fe0bff332bdfd12cc1
Dara Oseyemi <doseyemi@princeton.edu>
2022-07-27 18:00:19 -0400

    testing

--- longDiv.h
+++ longDiv.h
@@ -9,4 +9,4 @@
 
 /* performs long division on a polynomial dividend and a linear
 polynomial divisor and returns a polynomial quotient */
-inline void longDiv(Polynomial_t* poly, double* a_n, double root, double diff);
+inline __attribute__((always_inline)) void longDiv(Polynomial_t* poly, double* a_n, double root, double diff);
