d74bb58f1d09eb2f1719331bafef2a8e7699cd4b
Dara Oseyemi <doseyemi@princeton.edu>
2022-07-27 17:54:34 -0400

    testing

--- derivative.h
+++ derivative.h
@@ -9,5 +9,5 @@
 
 /* The derivative function takes in a Polynomial_t
 object as input and set differentiatedPoly to its derivative. */
-void derivative(Polynomial_t myPoly, Polynomial_t* differentiatedPoly);
+inline void derivative(Polynomial_t myPoly, Polynomial_t* differentiatedPoly);
 
