ea1abb37107e77c522f1ad75a014344059f33039
Dara Oseyemi <doseyemi@princeton.edu>
2022-07-27 17:01:46 -0400

    testing

--- newton.c
+++ newton.c
@@ -68,7 +68,42 @@ void newton(Polynomial_t poly, double* roots, double convCrit) {
     int rootIndex = 0;
     printf("test -1.5\n");
     while (poly.degree > 0) {
-        derivative(poly, &polyDeriv);
+        // derivative(poly, &polyDeriv);
+
+        // pointers for the resulting coefficients (after taking the derivative) and the
+        // original coefficients
+        polyDeriv.degree = myPoly.degree - 1;
+        double* results = polyDeriv.coefficients;
+        double* coeffs = myPoly.coefficients + 1; // the constant at index 0 is not included in the derivative
+
+        // array that just holds 1 to n, where n is the degree of the original polynomial.
+        // this represents the exponents of the polynomial
+        double indices[myPoly.degree];
+        for(int i = 0; i < myPoly.degree; i++){
+            indices[i] = i + 1;
+        }
+
+        //active vector length is the degree of the original polynomial
+        size_t avl = myPoly.degree;
+        // declare vector registers
+        vfloat64m1_t vCoeffs, vIndices, vResults;
+
+        for (size_t vl; (vl = vsetvl_e64m1(avl)) > 0; avl -= vl) {
+            // load in original coefficients into vCoeffs
+            vCoeffs = vle64_v_f64m1(coeffs, vl);
+            // load in array of exponents
+            vIndices = vle64_v_f64m1(indices, vl);
+            // multiply the two and put the result in vResults
+            vResults = vfmul_vv_f64m1(vCoeffs, vIndices, vl);
+            // store the resulting coefficients into our differentiated polynomial
+            vse64_v_f64m1(results, vResults, vl);
+
+            // move the pointers
+            coeffs += vl;
+            *indices += vl;
+            results += vl;
+        }
+
         // printf("test 0\n");
         // bool cond = true;
         // long cond = 0;
