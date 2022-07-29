34489fb574632b5ee189345ee7d07a5e0a407b64
Mukund Ramakrishnan <mramakri@outlook.com>
2022-07-28 23:46:52 -0400

    first round testing

--- main.c
+++ main.c
@@ -12,32 +12,32 @@
 
 int main() {
     Polynomial_t poly;
-    poly.degree = 2;
-    double arr[] = {-1,0,1};
+    poly.degree = 3;
+    double arr[] = {210, 107, 18, 1};
     poly.coefficients = arr;
 
     double roots[poly.degree];
     double crit_conversion = 1e-10;
 
-    printf("Polynomial:\n");
-    printPoly(poly);
+    // printf("Polynomial:\n");
+    // printPoly(poly);
 
     start_timer();
     newton(poly, roots, crit_conversion);
     stop_timer();
     int64_t cycle_time = get_timer();
 
-    if (poly.degree == 0 || roots[0] == DBL_MAX) {
-        printf("Your polynomial has no real roots.\n");
-    }
-    else {
-        for (int i = 0; i < poly.degree; i++) {
-            if (roots[i] == DBL_MAX) {
-                break;
-            }
-            printf("The root approximation is: %.18f\n", roots[i]);
-        }
-    }
+    // if (poly.degree == 0 || roots[0] == DBL_MAX) {
+    //     printf("Your polynomial has no real roots.\n");
+    // }
+    // else {
+    //     for (int i = 0; i < poly.degree; i++) {
+    //         if (roots[i] == DBL_MAX) {
+    //             break;
+    //         }
+    //         printf("The root approximation is: %.18f\n", roots[i]);
+    //     }
+    // }
 
     printf("\nRuntime (cycles): %d\n", cycle_time);
 
